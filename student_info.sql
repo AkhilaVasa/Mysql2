a97924c2fcb04d9b5e5eb61f21802573f68c75d2
AkhilaVasa <88317493+AkhilaVasa@users.noreply.github.com>
2021-12-07 21:24:23 +0530

    Add files via upload

---
+++ student_info.sql
@@ -0,0 +1,12 @@
+select * from users;
+select * from codekata;
+select * from company_drives;
+select * from mentors;
+select * from student_activated_course;
+select * from courses;
+select * from attendance;
+select * from tasks;
+select * from topics;
+
+
+
